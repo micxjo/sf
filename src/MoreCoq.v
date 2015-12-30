0d65c04fb7298fee35281d7d953cd8e421d66b17
Micxjo Funkcio <micxjo@fastmail.com>
2015-12-29 20:07:15 -0800

    Finish MoreCoq chapter

--- src/MoreCoq.v
+++ src/MoreCoq.v
@@ -300,3 +300,158 @@ Proof.
       inversion eq.
       reflexivity.
 Qed.
+
+Definition sillyfun (n:nat) : bool :=
+  if beq_nat n 3 then false
+  else if beq_nat n 5 then false
+       else false.
+
+Theorem sillyfun_false : forall (n:nat),
+                           sillyfun n = false.
+Proof.
+  intros n.
+  unfold sillyfun.
+  destruct (beq_nat n 3).
+  Case "beq_nat n 3 = true". reflexivity.
+  Case "beq_nat n 3 = false".
+    destruct (beq_nat n 5).
+    SCase "beq_nat n 5 = true". reflexivity.
+    SCase "beq_nat n 5 = false". reflexivity.
+Qed.
+
+Theorem override_shadow :
+  forall (X:Type) x1 x2 k1 k2 (f:nat->X),
+    (override (override f k1 x2) k1 x1) k2 = (override f k1 x1) k2.
+Proof.
+  intros X x1 x2 k1 k2 f.
+  unfold override.
+  destruct (beq_nat k1 k2).
+  Case "beq_nat k1 k2 = true". reflexivity.
+  Case "beq_nat k1 k2 = false". reflexivity.
+Qed.
+
+Theorem combine_split : forall X Y (l:list (X * Y)) l1 l2,
+                          split l = (l1, l2) ->
+                          combine l1 l2 = l.
+Proof.
+  intros X Y l l1 l2 eq.
+  generalize dependent l1.
+  generalize dependent l2.
+  induction l as [| [x y] l'].
+  Case "l = []".
+    intros l1 l2 eq.
+    inversion eq.
+    reflexivity.
+  Case "l = (x, y) :: l'".
+    intros l1 l2 eq.
+    destruct l1 as [|x' l1'].
+    SCase "l1 = []".
+      inversion eq.
+      destruct (split l') as [xs ys].
+      inversion H0.
+    SCase "l1 = x' :: l1'".
+      inversion eq.
+      destruct (split l') as [xs ys].
+      inversion H0.
+      simpl.
+      rewrite -> IHl'.
+      reflexivity.
+      rewrite -> H3.
+      reflexivity.
+Qed.
+
+Definition sillyfun1 (n:nat) : bool :=
+  if beq_nat n 3 then true
+  else if beq_nat n 5 then true
+       else false.
+
+Theorem sillyfun1_odd : forall (n:nat),
+                          sillyfun1 n = true ->
+                          oddb n = true.
+Proof.
+  intros n eq.
+  unfold sillyfun1 in eq.
+  destruct (beq_nat n 3) eqn:Heqe3.
+  Case "e3 = true".
+    apply beq_nat_true in Heqe3.
+    rewrite -> Heqe3.
+    reflexivity.
+  Case "e3 = false".
+    destruct (beq_nat n 5) eqn: Heqe5.
+    SCase "e5 = true".
+      apply beq_nat_true in Heqe5.
+      rewrite -> Heqe5.
+      reflexivity.
+    SCase "e5 = false". inversion eq.
+Qed.
+
+Theorem bool_fn_applied_thrice :
+  forall (f:bool -> bool) (b : bool),
+    f (f (f b)) = f b.
+Proof.
+  intros f b.
+  destruct (f b) eqn: Hfb.
+  Case "f b = true".
+    destruct b eqn: Hb.
+    SCase "b = true".
+      rewrite -> Hfb.
+      apply Hfb.
+    SCase "b = false".
+      destruct (f true) eqn: Hftrue.
+      SSCase "f true = true". apply Hftrue.
+      SSCase "f true = false". apply Hfb.
+  Case "f b = false".
+    destruct b eqn: Hb.
+    SCase "b = true".
+      destruct (f false) eqn: Hffalse.
+      SSCase "(f false) = tru". apply Hfb.
+      SSCase "(f false) = false". apply Hffalse.
+    SCase "b = false".
+      rewrite -> Hfb.
+      apply Hfb.
+Qed.
+
+Theorem override_same : forall (X:Type) x1 k1 k2 (f:nat->X),
+                          f k1 = x1 ->
+                          (override f k1 x1) k2 = f k2.
+Proof.
+  intros X x1 k1 k2 f eq.
+  unfold override.
+  destruct (beq_nat k1 k2) eqn: Hk1k2.
+  Case "beq_nat k1 k2 = true".
+    rewrite <- eq.
+    apply f_equal.
+    apply beq_nat_true in Hk1k2.
+    apply Hk1k2.
+  Case "beq_nat k1 k2 = false".
+    reflexivity.
+Qed.
+
+Theorem beq_nat_sym : forall (n m : nat),
+                        beq_nat n m = beq_nat m n.
+Proof.
+  intros n. induction n as [|n'].
+  Case "n = 0".
+    intros m. destruct m as [|m'].
+    SCase "m = 0". reflexivity.
+    SCase "m = S m'". reflexivity.
+  Case "n = S n'".
+    intros m. destruct m as [|m'].
+    SCase "m = 0". reflexivity.
+    SCase "m = S m'". simpl. apply IHn'.
+Qed.
+
+Theorem beq_nat_trans : forall n m p,
+                          beq_nat n m = true ->
+                          beq_nat m p = true ->
+                          beq_nat n p = true.
+Proof.
+  intros n m p eq1 eq2.
+  apply beq_nat_true in eq1.
+  apply beq_nat_true in eq2.
+  rewrite trans_eq with (n:=n) (m:=m) (o:=p).
+  symmetry.
+  apply beq_nat_refl.
+  apply eq1.
+  apply eq2.
+Qed.
